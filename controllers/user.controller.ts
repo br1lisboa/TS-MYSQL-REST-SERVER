a668e4d6e073cd982aa81bed6a21d75d32314a0a
br1lisboa <brlisbo@gmail.com>
2022-10-27 07:32:03 -0300

    finished crud ts node mysql

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,60 +1,147 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
 
-export const getUsers = (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
+export const getUsers = async (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
+
+    // Leer users de nuestra bd mysql
+    const users = await User.findAll();
 
     res.json({
-        msg: 'getUsers'
+        msg: 'All users',
+        users
     });
 
 };
 
 
-export const getUser = (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
+export const getUser = async (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
 
     const { id } = req.params;
 
-    res.json({
-        msg: 'getUser',
-        id
-    });
+    const user = await User.findByPk(id);
+
+    if (!user) {
+
+        res.status(404).json({
+            msg: `No existe un usuario con el id ${id}`
+        });
+
+    } else {
+
+        res.json({
+            msg: 'User by primary key',
+            user
+        });
+
+    };
 
 };
 
 
-export const postUser = (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
+export const postUser = async (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
 
     const { body } = req;
 
-    res.json({
-        msg: 'postUser',
-        body
-    });
+    try {
+
+        const isMailExist = await User.findOne({
+            where: {
+                mail: body.mail
+            }
+        });
+
+        if (isMailExist) {
+            return res.status(400).json({
+                msg: `User whit mail ${req.body.mail} alredy exist`
+            })
+        };
+
+        const nwUser = new User(body);
+        await nwUser.save();
+
+        res.json({
+            msg: 'New user:',
+            nwUser
+        });
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            msg: 'You need to contact whhit the admin.'
+        });
+
+    };
 
 };
 
 
-export const putUser = (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
+export const putUser = async (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
 
-    const { id } = req.params;
     const { body } = req;
+    const { id } = req.params;
 
-    res.json({
-        msg: 'putUser',
-        body,
-        id
-    });
+    try {
+
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).json({
+                msg: `User whit id ${id} isnt here`
+            })
+        };
+
+        await user.update(body);
+
+        res.json({
+            msg: 'User update:',
+            user
+        });
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            msg: 'You need to contact whhit the admin.'
+        });
+
+    };
 
 };
 
 
-export const deleteUser = (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
+export const deleteUser = async (req: Request, res: Response) => { //> Con : le indicamos el tipo, que lo requiere ts
 
     const { id } = req.params;
 
+    const user = await User.findByPk(id)
+
+    if (!user) {
+        return res.status(400).json({
+            msg: `User whit id ${id} inst in our BD`
+        })
+    };
+
+    // Eliminiacion LOGICA - mantiene la integridad fisica de nuestra BD
+
+    await user.update({ state: false });
+
     res.json({
-        msg: 'deleteUser',
-        id
-    });
+        msg: 'User destroy',
+        user
+    }); 
+
+
+    /* 
+    
+    ELIMINACION FISICA - SE PIERDE LA REFERENCIA INTEGRAL
+    await user.destroy();
+
+    res.json({
+        msg: 'User destroy',
+        user
+    }); 
+    
+    */
 
 };
--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -1,44 +1,127 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUser = exports.putUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
-const getUsers = (req, res) => {
+const user_1 = __importDefault(require("../models/user"));
+const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // Leer users de nuestra bd mysql
+    const users = yield user_1.default.findAll();
     res.json({
-        msg: 'getUsers'
+        msg: 'All users',
+        users
     });
-};
+});
 exports.getUsers = getUsers;
-const getUser = (req, res) => {
+const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
-    res.json({
-        msg: 'getUser',
-        id
-    });
-};
+    const user = yield user_1.default.findByPk(id);
+    if (!user) {
+        res.status(404).json({
+            msg: `No existe un usuario con el id ${id}`
+        });
+    }
+    else {
+        res.json({
+            msg: 'User by primary key',
+            user
+        });
+    }
+    ;
+});
 exports.getUser = getUser;
-const postUser = (req, res) => {
+const postUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    res.json({
-        msg: 'postUser',
-        body
-    });
-};
+    try {
+        const isMailExist = yield user_1.default.findOne({
+            where: {
+                mail: body.mail
+            }
+        });
+        if (isMailExist) {
+            return res.status(400).json({
+                msg: `User whit mail ${req.body.mail} alredy exist`
+            });
+        }
+        ;
+        const nwUser = new user_1.default(body);
+        yield nwUser.save();
+        res.json({
+            msg: 'New user:',
+            nwUser
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'You need to contact whhit the admin.'
+        });
+    }
+    ;
+});
 exports.postUser = postUser;
-const putUser = (req, res) => {
-    const { id } = req.params;
+const putUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { body } = req;
-    res.json({
-        msg: 'putUser',
-        body,
-        id
-    });
-};
-exports.putUser = putUser;
-const deleteUser = (req, res) => {
     const { id } = req.params;
+    try {
+        const user = yield user_1.default.findByPk(id);
+        if (!user) {
+            return res.status(404).json({
+                msg: `User whit id ${id} isnt here`
+            });
+        }
+        ;
+        yield user.update(body);
+        res.json({
+            msg: 'User update:',
+            user
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'You need to contact whhit the admin.'
+        });
+    }
+    ;
+});
+exports.putUser = putUser;
+const deleteUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const user = yield user_1.default.findByPk(id);
+    if (!user) {
+        return res.status(400).json({
+            msg: `User whit id ${id} inst in our BD`
+        });
+    }
+    ;
+    // Eliminiacion LOGICA - mantiene la integridad fisica de nuestra BD
+    yield user.update({ state: false });
     res.json({
-        msg: 'deleteUser',
-        id
+        msg: 'User destroy',
+        user
     });
-};
+    /*
+    
+    ELIMINACION FISICA - SE PIERDE LA REFERENCIA INTEGRAL
+    await user.destroy();
+
+    res.json({
+        msg: 'User destroy',
+        user
+    });
+    
+    */
+});
 exports.deleteUser = deleteUser;
 //# sourceMappingURL=user.controller.js.map
